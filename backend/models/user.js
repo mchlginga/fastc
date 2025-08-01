2ee3f120024c736535074ea1b9de663a22e49f72
mchl <mchlginga@gmail.com>
2025-08-01 10:58:28 +0800

    feat: add User mongoose model

---
+++ backend/models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    skills: {
+        type: [String],
+        default: []
+    },
+
+    certificates: [
+        {
+            name: String,
+            issuedAt: Date,
+            expiresAt: Date
+        }
+    ],
+
+    roles: {
+        type: String,
+        enum: ["admin", "company", "user"],
+        default: "user"
+    },
+
+    profilePic: {
+        type: String,
+        default: ""
+    }
+
+}, {
+    timestamps: true
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.gensalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+userSchema.methods.matchPassword = (async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
