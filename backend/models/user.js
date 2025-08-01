a09c74a36cb3bf1a23004687f2a23096f3b17b54
mchl <mchlginga@gmail.com>
2025-08-01 15:59:20 +0800

    fix: correct typo in User model's role field

--- backend/models/user.js
+++ backend/models/user.js
@@ -33,7 +33,7 @@ const userSchema = new mongoose.Schema ({
         }
     ],
 
-    roles: {
+    role: {
         type: String,
         enum: ["admin", "company", "user"],
         default: "user"
@@ -53,7 +53,7 @@ userSchema.pre("save", async function (next) {
         return next();
     }
 
-    const salt = await bcrypt.gensalt(10);
+    const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 
     next();
