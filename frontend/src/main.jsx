1d6425ed39c96be997932baac1a7b87057f53b4e
mchl <mchlginga@gmail.com>
2025-08-06 16:41:28 +0800

    refactor: clean up default Vite boilerplate

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,10 @@
+// di ko pa inaalam anong gingagawa neto
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
 
+// di ko pa inaalam anong gingagawa neto
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
