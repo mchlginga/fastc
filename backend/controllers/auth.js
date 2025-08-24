ac34d4cf1a26941024b58f4868db2cc7d851eb00
mchl <mchlginga@gmail.com>
2025-08-24 14:42:29 +0800

    feat: implement authentication with secure cookies

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,21 @@
 const User = require("../models/user");
 const { statusCodes } = require("../utils/constant");
 const generateToken = require("../utils/generateToken");
+const config = require("../config/index");
+
+// cookie config
+const COOKIE_NAME = "token";
+const cookieOptions = {
+    httpOnly: true,
+    secure: config.env === "production",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 1000
+};
+
+const setCookieToken = (res, userId) => {
+    const token = generateToken(userId);
+    res.cookie(COOKIE_NAME, token, cookieOptions);
+};
 
 exports.register = async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -14,12 +29,13 @@ exports.register = async (req, res, next) => {
 
         const user = await User.create({ name, email, password });
 
+        setCookieToken(res, user.id);
+
         res.status(statusCodes.CREATED).json({
             _id: user.id,
             name: user.name,
             email: user.email,
             role: user.role,
-            token: generateToken(user.id)
         });
     } catch (error) {
         next(error);
@@ -36,6 +52,8 @@ exports.login = async (req, res, next) => {
             res.status(statusCodes.UNAUTHORIZED).json({ message: "Invalid Email or Password"} );
         }
 
+        setCookieToken(res, user.id);
+        
         res.status(statusCodes.OK).json({
             _id: user.id,
             name: user.name,
@@ -43,6 +61,19 @@ exports.login = async (req, res, next) => {
             role: user.role,
             token: generateToken(user.id)
         });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.logout = (req, res, next) => {
+    try {
+        res.clearCookie(COOKIE_NAME, {
+            ...cookieOptions,
+            maxAge: 0
+        });
+
+        res.status(statusCodes.OK).json({ message: "Logged out successfully"});
     } catch (error) {
         next(error);
     }
