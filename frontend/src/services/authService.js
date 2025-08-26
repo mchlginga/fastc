2a12b739b3741602a17f95124a8d9ddc41a441f9
mchl <mchlginga@gmail.com>
2025-08-26 17:25:47 +0800

    feat: add frontend authentication service

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,15 @@
+import { api } from "./api";
+
+export const login = async(email, password) => {
+    const { data } = await api.post("/auth/login", { email, password });
+    return data;
+};
+
+export const getMe = async() => {
+    const { data } = await api.get("/auth/me");
+    return data.user;
+};
+
+export const logout = async() => {
+    await api.post("auth/logout");
+};
