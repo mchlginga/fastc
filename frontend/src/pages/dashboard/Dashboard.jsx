375b9b5c6de78d5cb4c93625beaee616d238b3ea
mchl <mchlginga@gmail.com>
2025-08-27 21:04:26 +0800

    feat: add logout button to dashboard

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+import { logout } from "../../services/authService";
+import Button from "../../components/Button";
+
 const Dashboard = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logout();
+        navigate("/login");
+    };
+
     return(
         <div>
             <h1>Hello, World!</h1>
+            <Button onClick={handleLogout}>Logout</Button>
         </div>
     );
 };
