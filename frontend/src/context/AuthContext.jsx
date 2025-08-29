3d92388105003767716f7f45a6bb651dd1f9b248
mchl <mchlginga@gmail.com>
2025-08-29 16:20:29 +0800

    feat: add AuthContext for global state management

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+
+import { getMe, logout } from "../services/authService";
+
+export const AuthContext = createContext();
+
+export function AuthProvider() {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        (async () => {
+
+            try {
+                const u = await getMe();
+                setUser(u);
+            } catch (error) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        })();
+    }, []);
+};
+
+const handleLogout = async () => {
+    await logout();
+    setUser(null);
+};
+
+return(
+    <AuthContext.Provider value={{ user, loading, handleLogout}}>
+        {!loading && children}
+    </AuthContext.Provider>
+);
