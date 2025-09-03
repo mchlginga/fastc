e15f315cedb57452b3e8f18bef26f029dbb09eb0
unknown <mchlginga@gmail.com>
2025-09-03 18:03:34 +0800

    feat: add and configure role-based routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Unauthorized from "./pages/errors/Unauthorized";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Public Routes */}
                 <Route path="/login" element={ <Login /> } />
+
+                <Route path="/unauthorized" element={ <Unauthorized /> } />
+
+                
+                {/* Trainee-only Routes */}
                 <Route 
                     path="/dashboard" 
                     element={ 
-                        <ProtectedRoute>
+                        <ProtectedRoute allowedRoles={["user", "admin"]}>
                             <Dashboard />
                         </ProtectedRoute>
                     } 
                 />
+
+                {/* Admin-only Routes */}
+
+                {/* Company-only Routes */}
+
             </Routes>
         </BrowserRouter>
     );
