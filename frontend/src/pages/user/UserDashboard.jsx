c31c9ac6138d5fc26751a2aef0c8d64ae09eb4ab
unknown <mchlginga@gmail.com>
2025-09-05 16:59:17 +0800

    feat: create role-specific dashboard components

---
+++ frontend/src/pages/user/UserDashboard.jsx
@@ -0,0 +1,15 @@
+import { useAuth } from "../../context/AuthContext";
+// import { AuthContext } from "../../context/AuthContext";
+import Button from "../../components/Button";
+
+export default function TraineeDashboard() {
+    const { user, handleLogout } = useAuth();
+    
+    return (
+        <div>
+            <h1>Welcome, {user?.name}!</h1>
+            <p>Your role: {user?.role}</p>
+            <Button onClick={handleLogout}>Logout</Button>
+        </div>
+    );
+};
