7c5e324821474d07425e46805f14224295c93b89
mchl <mchlginga@gmail.com>
2025-09-07 17:59:29 +0800

    feat: add user registration service function

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,11 +2,28 @@ import { api } from "./api";
 
 export const login = async(email, password) => {
     const { data } = await api.post("/auth/login", { email, password });
+
+    return data;
+};
+
+export const register = async({ username, firstName, surname, email, password, city, country, privacyAgreement }) => {
+    const { data } = await api.post("/auth/register", {
+        username,
+        firstName,
+        surname,
+        email,
+        password,
+        city,
+        country,
+        privacyAgreement
+    });
+
     return data;
 };
 
 export const getMe = async() => {
     const { data } = await api.get("/auth/me");
+    
     return data.user;
 };
 
