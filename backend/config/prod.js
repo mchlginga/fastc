ae5a9471a1ac3b382f44265d05a369fcb3eb7596
mchl <mchlginga@gmail.com>
2025-09-08 17:30:38 +0800

    feat: add email service configuration

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -4,5 +4,11 @@ module.exports = {
     dbName: process.env.DB_NAME || "fastc_prod",
     mongoUri: process.env.MONGO_URI,
     jwtSecret: process.env.JWT_SECRET,
-    jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1d"
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN || "1d",
+    frontendUrl: process.env.FRONTEND_URL,
+    email: {
+        user: process.env.EMAIL_SERVICE,
+        pass: process.env.EMAIL_PASS,
+        service: process.env.EMAIL_SERVICE
+    }
 }; 
