08c9afed0c5f7bea40724dd925780e8e730eeafe
unknown <mchlginga@gmail.com>
2025-09-12 18:06:09 +0800

    feat: refactor certificate generation to save and simplify logic

--- backend/controllers/certificate.js
+++ backend/controllers/certificate.js
@@ -12,22 +12,24 @@ exports.generateCert = async (req, res, next) => {
             return res.status(statusCodes.NOT_FOUND).json({ message: "User not found." });
         }
 
-        // certificate exists in user's certificates?
-        const certificate = user.certificates.find(cert => cert.name === certificateName);
-        if (!certificate) {
-            return res.status(statusCodes.BAD_REQUEST).json({ message: "Certificate not found." });
-        }
-
         const pdf = await generateCert({
             name: user.name,
-            certificateName,
-            date: certificate.issuedAt ? new Date(certificate.issuedAt).
-            toLocaleDateString() : new Date().toLocaleDateString()
+            certificateName: certificateName || "Course Completion",
+            date: new Date().toLocaleDateString()
         });
+
+        if(certificateName) {
+            user.certificates.push({
+                name: certificateName,
+                issuedAt: new Date()
+            });
+
+            await user.save();
+        }
 
         res.set({
             "Content-Type": "application/pdf",
-            "Content-Disposition": `filename=${user.name}-certificate.pdf`
+            "Content-Disposition": `filename=${user.name}-${certificateName || "certificate.pdf"}`
         });
 
         res.send(pdf);
