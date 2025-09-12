965b4ce95d0a8101d03715e701edddeeccfba463
unknown <mchlginga@gmail.com>
2025-09-12 16:56:55 +0800

    feat: enhance certificate generation with validation and data

--- backend/controllers/certificate.js
+++ backend/controllers/certificate.js
@@ -4,17 +4,25 @@ const generateCert = require("../utils/generateCert");
 
 exports.generateCert = async (req, res, next) => {
     const userId = req.user.id;
+    const { certificateName } = req.query;
 
     try {
         const user = await User.findById(userId);
-
         if (!user) {
-            res.status(statusCodes.NOT_FOUND).json({ message: "User not found."});
+            return res.status(statusCodes.NOT_FOUND).json({ message: "User not found." });
+        }
+
+        // certificate exists in user's certificates?
+        const certificate = user.certificates.find(cert => cert.name === certificateName);
+        if (!certificate) {
+            return res.status(statusCodes.BAD_REQUEST).json({ message: "Certificate not found." });
         }
 
         const pdf = await generateCert({
             name: user.name,
-            date: new Date().toLocaleDateString()
+            certificateName,
+            date: certificate.issuedAt ? new Date(certificate.issuedAt).
+            toLocaleDateString() : new Date().toLocaleDateString()
         });
 
         res.set({
