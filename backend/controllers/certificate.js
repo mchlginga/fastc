2d12b21dee1c2a44787a6163d32ca6c2e45adac4
mchl <mchlginga@gmail.com>
2025-09-14 14:22:19 +0800

    feat: refactor certificate generation to use Completion model

--- backend/controllers/certificate.js
+++ backend/controllers/certificate.js
@@ -1,10 +1,11 @@
 const User = require("../models/user");
+const Completion = require("../models/completion");
 const { statusCodes } = require("../utils/constant");
 const generateCert = require("../utils/generateCert");
 
 exports.generateCert = async (req, res, next) => {
     const userId = req.user.id;
-    const { certificateName } = req.query;
+    const { courseId } = req.query;
 
     try {
         const user = await User.findById(userId);
@@ -12,24 +13,23 @@ exports.generateCert = async (req, res, next) => {
             return res.status(statusCodes.NOT_FOUND).json({ message: "User not found." });
         }
 
+        const completion = await Completion.findOne({ 
+            user: userId,
+            course: courseId
+        }).populate("course");
+        if (!completion) {
+            return res.status(statusCodes.BAD_REQUEST).json({ message: "Course completion not found."});
+        }
+
         const pdf = await generateCert({
             name: user.name,
-            certificateName: certificateName || "Course Completion",
-            date: new Date().toLocaleDateString()
+            certificateName: completion.course.name,
+            date: completion.completedAt.toLocaleDateString()
         });
-
-        if(certificateName) {
-            user.certificates.push({
-                name: certificateName,
-                issuedAt: new Date()
-            });
-
-            await user.save();
-        }
 
         res.set({
             "Content-Type": "application/pdf",
-            "Content-Disposition": `filename=${user.name}-${certificateName || "certificate.pdf"}`
+            "Content-Disposition": `filename=${user.name}-${completion.course.name}.pdf`
         });
 
         res.send(pdf);
