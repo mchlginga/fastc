83594085b1504f16a6cfec5f62eca2b103520869
mchl <mchlginga@gmail.com>
2025-09-28 00:02:00 +0800

    refactor(completion): improve code formatting and readability in completion controller

--- backend/controllers/completion.js
+++ backend/controllers/completion.js
@@ -2,25 +2,25 @@ const Completion = require("../models/completion");
 const { statusCodes } = require("../utils/constant");
 
 exports.getCompletions = async (req, res, next) => {
-    const { user } = req.query;
+  const { user } = req.query;
 
-    try {
-        const query = req.user.role === "admin" ? {} : { user };
-        const completions = await Completion.find(query).populate("user course");
-        res.status(statusCodes.OK).json(completions);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const query = req.user.role === "admin" || req.user.role === "company" ? {} : { user };
+    const completions = await Completion.find(query).populate("user course");
+    res.status(statusCodes.OK).json(completions);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.createCompletions = async (req, res, next) => {
-    const { user, course } = req.body;
+  const { user, course } = req.body;
 
-    try {
-        const completion = await Completion.create({ user, course });
-        const populatedCompletion = await Completion.findById(completion._id).populate("user course")
-        res.status(statusCodes.CREATED).json(populatedCompletion);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    const completion = await Completion.create({ user, course });
+    const populatedCompletion = await Completion.findById(completion._id).populate("user course");
+    res.status(statusCodes.CREATED).json(populatedCompletion);
+  } catch (error) {
+    next(error);
+  }
 };
