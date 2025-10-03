6a333a85cfba9950bd2a32040c87275cae1efa0e
unknown <mchlginga@gmail.com>
2025-10-03 19:23:40 +0800

    update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
-
 const config = require("./index");
 
 const connectDB = async () => {
     try {
+        console.log("🔌 Trying to connect to:", config.mongoUri); // Debug log
         const conn = await mongoose.connect(config.mongoUri);
-        console.log("MongoDB successfully connected.");
+
+        // Log the actual DB name
+        console.log("✅ MongoDB connected to:", conn.connection.name);
     } catch (error) {
-        console.error("MongoDB connection failed:", error);
+        console.error("❌ MongoDB connection failed:", error);
         process.exit(1);
     }
 };
