c64107f5ba29fb9810c3e6edea138e3a81ef9086
unknown <mchlginga@gmail.com>
2025-10-05 02:22:50 +0800

    hey

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -7,13 +7,10 @@ export const login = async (email, password, rememberMe = false) => {
             password,
             rememberMe,
         });
-        if (!data.publicUser || !data.token) {
+        if (!data.publicUser) {
             throw new Error("Unexpected response format from server.");
         }
-        document.cookie = `token=${data.token}; path=/; max-age=${
-            rememberMe ? 30 * 24 * 60 * 60 : 7 * 24 * 60 * 60
-        }; SameSite=None; Secure`;
-        return { publicUser: data.publicUser, token: data.token };
+        return data.publicUser;
     } catch (error) {
         throw new Error(
             error.response?.data?.message ||
@@ -29,37 +26,38 @@ export const register = async ({
     password,
     privacyAgreement,
 }) => {
-    try {
-        const { data } = await api.post("/auth/register", {
-            firstName,
-            surname,
-            email,
-            password,
-            privacyAgreement,
-        });
-        if (!data.publicUser || !data.token) {
-            throw new Error("Unexpected response format from server.");
-        }
-        document.cookie = `token=${data.token}; path=/; max-age=${
-            7 * 24 * 60 * 60
-        }; SameSite=None; Secure`;
-        return { publicUser: data.publicUser, token: data.token };
-    } catch (error) {
-        throw new Error(
-            error.response?.data?.message || "Registration failed."
-        );
-    }
+    const { data } = await api.post("/auth/register", {
+        firstName,
+        surname,
+        email,
+        password,
+        privacyAgreement,
+    });
+    return data.publicUser;
+};
+
+export const registerUser = async ({
+    firstName,
+    surname,
+    email,
+    password,
+    role,
+    privacyAgreement,
+}) => {
+    const { data } = await api.post("/user", {
+        firstName,
+        surname,
+        email,
+        password,
+        role,
+        privacyAgreement,
+    });
+    return data.publicUser;
 };
 
 export const getMe = async () => {
-    try {
-        const { data } = await api.get("/auth/me");
-        return data.user;
-    } catch (error) {
-        throw new Error(
-            error.response?.data?.message || "Failed to fetch user data."
-        );
-    }
+    const { data } = await api.get("/auth/me");
+    return data.user;
 };
 
 export const logout = async () => {
